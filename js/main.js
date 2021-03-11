ae297d18ad080ceaebb6e1eabe287859a7ee9689
Tomas Kaiser <tomas.kaiser06@gmail.com>
2021-03-11 22:01:54 +0100

    Add intersection for navbar

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ navCollapsible.addEventListener("click", function () {
     .querySelector(".nav-collapsible__content")
     .classList.toggle("collapsible--expanded");
 });
+
+// Navbar intersection
+const nav = document.querySelector(".nav");
+const hero = document.querySelector(".hero");
+
+const heroOptions = {
+  rootMargin: "-500px 0px 0px 0px",
+};
+
+const heroObserver = new IntersectionObserver(function (entries, heroObserver) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      nav.classList.add("nav-scrolled");
+    } else {
+      nav.classList.remove("nav-scrolled");
+    }
+  });
+}, heroOptions);
+
+heroObserver.observe(hero);
