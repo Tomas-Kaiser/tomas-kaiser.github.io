815aadf80c5f90b715eb111bb4e2b5ad22a70542
Tomas Kaiser <tomas.kaiser06@gmail.com>
2021-03-17 17:08:14 +0100

    Add animation effects

--- js/main.js
+++ js/main.js
@@ -146,3 +146,32 @@ const contactObserver = new IntersectionObserver(function (
 contactOption);
 
 contactObserver.observe(contact);
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-left");
+
+const appearOptions = {
+  rootMargin: "0px 0px -100px 0px",
+};
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
